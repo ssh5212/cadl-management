96b20bbb42fd0c63b0b59f4f08acc157e5e95cb0
AngelPlayer <ssh5212@naver.com>
2022-12-27 11:45:31 +0900

    apply material ui

--- src/App.js
+++ src/App.js
@@ -1,62 +1,161 @@
 import './App.css';
 import Customer from './components/Customer';
 import { Component } from 'react';
+import {
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Paper,
+} from '@mui/material';
 
 const customers = [
     {
         id: 1,
-        image: 'https://placeimg.com/64/64/1',
-        name: '나동ssss',
-        birthday: '970808',
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
         gender: '남자',
-        job: ' 대학생',
+        etc: '-',
     },
     {
         id: 2,
-        image: 'https://placeimg.com/64/64/2',
-        name: '홍길',
-        birthday: '970808',
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
         gender: '남자',
-        job: ' 대학생',
+        etc: '-',
     },
     {
         id: 3,
-        image: 'https://placeimg.com/64/64/3',
-        name: '이순',
-        birthday: '970808',
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
         gender: '남자',
-        job: ' 대학생',
+        etc: '-',
+    },
+    {
+        id: 3,
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
+        gender: '남자',
+        etc: '-',
+    },
+    {
+        id: 3,
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
+        gender: '남자',
+        etc: '-',
+    },
+    {
+        id: 3,
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
+        gender: '남자',
+        etc: '-',
+    },
+    {
+        id: 3,
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
+        gender: '남자',
+        etc: '-',
+    },
+    {
+        id: 3,
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
+        gender: '남자',
+        etc: '-',
+    },
+    {
+        id: 3,
+        name: '신승헌',
+        studentId: '201621751',
+        department: '컴공',
+        gender: '남자',
+        etc: '-',
     },
 ];
 
 class App extends Component {
     render() {
         return (
-            <div>
-                {customers.map(c => {
-                    return (
-                        <Customer
-                            key={c.id}
-                            id={c.id}
-                            image={c.image}
-                            name={c.name}
-                            birthday={c.birthday}
-                            gender={c.gender}
-                            job={c.job}
-                        />
-                    );
-                })}
-
-                {/* <Customer
-                    id={customers[0].id}
-                    image={customers[0].image}
-                    name={customers[0].name}
-                    birthday={customers[0].birthday}
-                    gender={customers[0].gender}
-                    job={customers[0].job}
-                /> */}
-            </div>
+            <TableContainer
+                component={Paper}
+                sx={{
+                    maxHeight: '300px',
+                    maxWidth: '1080px',
+                }}
+            >
+                <Table stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align='center'>번호</TableCell>
+                            <TableCell align='center'>이름</TableCell>
+                            <TableCell align='center'>학번</TableCell>
+                            <TableCell align='center'>학과</TableCell>
+                            <TableCell align='center'>성별</TableCell>
+                            <TableCell align='center'>기타</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {customers.map(c => {
+                            return (
+                                <Customer
+                                    key={c.id}
+                                    id={c.id}
+                                    name={c.name}
+                                    studentId={c.studentId}
+                                    department={c.department}
+                                    gender={c.gender}
+                                    etc={c.etc}
+                                />
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         );
+        // return (
+        //     <Paper className='classes.root'>
+        //         <Table className='classes.table'>
+        // <TableHead>
+        //     <TableRow>
+        //         <TableCell>번호</TableCell>
+        //         <TableCell>이름</TableCell>
+        //         <TableCell>학번</TableCell>
+        //         <TableCell>학과</TableCell>
+        //         <TableCell>성별</TableCell>
+        //         <TableCell>기타</TableCell>
+        //     </TableRow>
+        // </TableHead>
+        // <TableBody>
+        //     {customers.map(c => {
+        //         return (
+        //             <Customer
+        //                 key={c.id}
+        //                 id={c.id}
+        //                 name={c.name}
+        //                 studentId={c.studentId}
+        //                 department={c.department}
+        //                 gender={c.gender}
+        //                 etc={c.etc}
+        //             />
+        //         );
+        //     })}
+        // </TableBody>
+        //         </Table>
+        // </Paper>
+        // );
     }
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,45 +1,18 @@
 import React from 'react';
+import { TableRow } from '@mui/material';
+import { TableCell } from '@mui/material';
 
 class Customer extends React.Component {
     render() {
         return (
-            <div>
-                <CustomerProfile
-                    id={this.props.id}
-                    image={this.props.image}
-                    name={this.props.name}
-                />
-                <CustomerInfo
-                    birthday={this.props.birthday}
-                    gender={this.props.gender}
-                    job={this.props.job}
-                />
-            </div>
-        );
-    }
-}
-
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt='profile' />
-                <h2>
-                    {this.props.name}({this.props.id})
-                </h2>
-            </div>
-        );
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell align='center'>{this.props.id}</TableCell>
+                <TableCell align='center'>{this.props.name}</TableCell>
+                <TableCell align='center'>{this.props.studentId}</TableCell>
+                <TableCell align='center'>{this.props.department}</TableCell>
+                <TableCell align='center'>{this.props.gender}</TableCell>
+                <TableCell align='center'>{this.props.etc}</TableCell>
+            </TableRow>
         );
     }
 }
