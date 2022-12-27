18952cb1773e8da49b36045bd8c94393a5eb5f79
AngelPlayer <ssh5212@naver.com>
2022-12-27 19:07:02 +0900

    add REST API module

--- client/src/App.js
+++ client/src/App.js
@@ -11,83 +11,33 @@ import {
     Paper,
 } from '@mui/material';
 
-const customers = [
-    {
-        id: 1,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 2,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 3,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 3,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 3,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 3,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 3,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 3,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-    {
-        id: 3,
-        name: '신승헌',
-        studentId: '201621751',
-        department: '컴공',
-        gender: '남자',
-        etc: '-',
-    },
-];
-
 class App extends Component {
+    // 서버에 접근하여 데이터를 받는 기능
+    state = {
+        customers: '',
+    };
+
+    // 모든 컴포넌트가 마운트 완료 되었을 때 실행
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ customers: res }))
+            .catch(err => console.log('Com', err));
+    }
+
+    // 비동기적으로 데이터를 접근하고 값을 반환
+    callApi = async () => {
+        const response = await fetch('/api/customers', {
+            headers: {
+                Accept: 'application / json',
+            },
+        });
+        const body = await response.json();
+        console.log('hola');
+        return body;
+    };
+
     render() {
+        const { classes } = this.props;
         return (
             <TableContainer
                 component={Paper}
@@ -108,19 +58,21 @@ class App extends Component {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {customers.map(c => {
-                            return (
-                                <Customer
-                                    key={c.id}
-                                    id={c.id}
-                                    name={c.name}
-                                    studentId={c.studentId}
-                                    department={c.department}
-                                    gender={c.gender}
-                                    etc={c.etc}
-                                />
-                            );
-                        })}
+                        {this.state.customers
+                            ? this.state.customers.map(c => {
+                                  return (
+                                      <Customer
+                                          key={c.id}
+                                          id={c.id}
+                                          name={c.name}
+                                          studentId={c.studentId}
+                                          department={c.department}
+                                          gender={c.gender}
+                                          etc={c.etc}
+                                      />
+                                  );
+                              })
+                            : ''}
                     </TableBody>
                 </Table>
             </TableContainer>
--- server.js
+++ server.js
@@ -6,8 +6,81 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.unsubscribe(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express' });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 2,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 3,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 3,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 3,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 3,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 3,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 3,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+        {
+            id: 3,
+            name: '신승헌',
+            studentId: '201621751',
+            department: '컴공',
+            gender: '남자',
+            etc: '-',
+        },
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
